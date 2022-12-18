c741f71e2ca7d31b5b69d3140d85a8d664252999
Felix Kloft <felix.kloft@gmail.com>
2022-12-18 23:22:38 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+document.addEventListener("mouseover", function() {
+	if(event.target.nodeName.toLowerCase() == "code") {
+		event.target.title = "Click to copy";
+	}
+});
+
+document.addEventListener("click", function() {
+	if(event.target.nodeName.toLowerCase() == "code") {
+		var text = event.target.textContent;
+		navigator.clipboard.writeText(text);
+		alert('"' + text + '" was copied to the clipboard.');
+	}
+});
+
+addEventListener("load", function() {
+	document.body.classList.add("click2copy");
+});
+
